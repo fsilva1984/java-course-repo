7183095ff7024e6d2a0e3b4947ecea52472fef6e
fsilva1984 <fsilva916@hotmail.com>
2024-10-21 16:10:54 -0300

    java basic part I

---
+++ learning-java/src/edu/basic/primitiveType/Main.java
@@ -0,0 +1,13 @@
+package edu.basic.primitiveType;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("\nTipos primitivos do java\n" +
+        "\tInteiros\n- Byte = 1 bite\n- Short = 2 bites\n- Int = 4 bites\n- Long = 8 bites" +
+        "\n\tDecimais ou Ponto Flutuante\n- Float\n- Double" +
+        "\n\tPrimitivo que representa apenas um caractere entre aspas simples" +
+        "\n- Char = '?'" +
+        "\n\tBoolean" +
+        "\n- True\n- False");
+  }
+}
