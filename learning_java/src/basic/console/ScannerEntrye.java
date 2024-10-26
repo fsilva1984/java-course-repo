c8a43b6bdeecb2ba177e16082282238dc9b48974
fsilva1984 <fsilva916@hotmail.com>
2024-10-26 11:26:21 -0300

    jav basic, console entrye

---
+++ learning_java/src/basic/console/ScannerEntrye.java
@@ -0,0 +1,26 @@
+package basic.console;
+
+import java.util.Scanner;
+
+public class ScannerEntrye {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.printf("\nNome: ");
+    String name = sc.nextLine();
+
+    System.out.printf("\nSobre nome : ");
+    String lastname = sc.nextLine();
+
+    System.out.printf("\nIdade : ");
+    int age = sc.nextInt();
+
+    String userinfo = String.format(
+        "\nnome: %s\nsobrenome: %s\nidade: %d", name, lastname, age);
+    System.out.println(userinfo);
+
+    sc.close();
+  }
+}
--- learning_java/src/basic/primitiveType/Part_Two.java
+++ learning_java/src/basic/primitiveType/Part_Two.java
@@ -4,25 +4,31 @@ public class Part_Two {
 
 	public static void main(String[] args) {
 		// exemplos de tipos primitivos numericos
-		
-		//inteiros
+
+		// inteiros
 		byte anosDeImpresa = 32;
 		short numerosDeVoos = 542;
 		int id = 23456;
 		long pontosAcumulados = 3_345_543_34L;
-		
+
 		// tipos numericos com casas decimais
 		float salario = 11_445.22F;
 		double numerodeVedas = 2_991_678_534.03;
-		
-		// tipos booleanos 
+
+		// tipos booleanos
 		boolean estaDeFerias = false; // ou true
-		
+
 		// tipo caractere
 		char status = 'A';
-		
-		
-		
+
+		System.out.println(anosDeImpresa);
+		System.out.println(numerosDeVoos);
+		System.out.println(id);
+		System.out.println(pontosAcumulados);
+		System.out.println(salario);
+		System.out.println(numerodeVedas);
+		System.out.println(estaDeFerias);
+		System.out.println(status);
 	}
 
 }
